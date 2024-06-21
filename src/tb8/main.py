d61ef22f5dd35866b258403ce603f32ef834ce1e
Louis Maddox <louismmx@gmail.com>
2024-06-21 21:36:42 +0100

    Initial commit

---
+++ src/tb8/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+port = int(os.environ.get("PORT", 4000))
+
+@app.get("/")
+def read_root():
+    return {"Hello": "world"}
+
+def hello():
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
