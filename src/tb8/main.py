5a9401cb6f854f5cf34a4aa96dfc3e7665fd1fe6
Louis Maddox <louismmx@gmail.com>
2024-06-22 00:38:13 +0100

    it's time for the tubeulator

--- src/tb8/main.py
+++ src/tb8/main.py
@@ -8,6 +8,11 @@ port = int(os.environ.get("PORT", 4000))
 def read_root():
     return {"Hello": "world"}
 
+@app.get("/tubeulate")
+def read_root():
+    import tubeulator
+    return {"Hello": "world"}
+
 def serve():
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
