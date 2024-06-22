ed0cf1fce937a6b8b01982e4070d93430d6ed2c2
Louis Maddox <louismmx@gmail.com>
2024-06-22 13:37:31 +0100

    Add SQL queries

--- src/tb8/main.py
+++ src/tb8/main.py
@@ -1,29 +1,33 @@
-from fastapi import FastAPI
 import os
-import tubeulator
+
+import tubeulator as tube
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 port = int(os.environ.get("PORT", 4000))
+
 
 @app.get("/")
 def read_root():
     return {"🚨": "It's time for the tubeulator"}
 
+
 @app.get("/lines")
-def read_tubeulate_lines():
-    lines = tubeulator.load_lines()
-    return lines.to_dicts()
+def read_lines(request: Request, query: str = "SELECT * FROM self;"):
+    return tube.load_lines().sql(query).to_dicts()
+
 
 @app.get("/lines-by-station")
-def read_tubeulate_lines_by_station():
-    lines = tubeulator.load_lines_by_station()
-    return lines.to_dicts()
+def read_lines_by_station(request: Request, query: str = "SELECT * FROM self;"):
+    return tube.load_lines_by_station().sql(query).to_dicts()
+
 
 @app.get("/stations")
-def read_tubeulate_stations():
-    stations = tubeulator.load_stations()
-    return stations.to_dicts()
+def read_stations(request: Request, query: str = "SELECT * FROM self;"):
+    return tube.load_stations().sql(query).to_dicts()
+
 
 def serve():
     import uvicorn
+
     uvicorn.run(app, host="0.0.0.0", port=port)
