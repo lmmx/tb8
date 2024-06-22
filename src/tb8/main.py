6dde92f75ed31b3f6372e25f591698d5b248c063
Louis Maddox <louismmx@gmail.com>
2024-06-22 16:34:27 +0100

    add CORS

--- src/tb8/main.py
+++ src/tb8/main.py
@@ -3,9 +3,17 @@ from datetime import datetime, timedelta, timezone
 
 import tubeulator as tube
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import AwareDatetime, BaseModel, ConfigDict, model_validator
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 port = int(os.environ.get("PORT", 4000))
 
 lines = tube.load_lines()
@@ -49,28 +57,36 @@ def read_root():
 def read_lines(request: Request, query: str = "SELECT * FROM self;"):
     received = time_now()
     results = lines.sql(query).to_dicts()
-    return Response(context=MetaData(request_time=received, query=query), results=results)
+    return Response(
+        context=MetaData(request_time=received, query=query), results=results
+    )
 
 
 @app.get("/lines-by-station")
 def read_lines_by_station(request: Request, query: str = "SELECT * FROM self;"):
     received = time_now()
     results = lines_by_station.sql(query).to_dicts()
-    return Response(context=MetaData(request_time=received, query=query), results=results)
+    return Response(
+        context=MetaData(request_time=received, query=query), results=results
+    )
 
 
 @app.get("/stations")
 def read_stations(request: Request, query: str = "SELECT * FROM self;"):
     received = time_now()
     results = stations.sql(query).to_dicts()
-    return Response(context=MetaData(request_time=received, query=query), results=results)
+    return Response(
+        context=MetaData(request_time=received, query=query), results=results
+    )
 
 
 @app.get("/station-points")
 def read_station_points(request: Request, query: str = "SELECT * FROM self;"):
     received = time_now()
     results = station_points.sql(query).to_dicts()
-    return Response(context=MetaData(request_time=received, query=query), results=results)
+    return Response(
+        context=MetaData(request_time=received, query=query), results=results
+    )
 
 
 def serve():
