648c74be8c3a268264a78c28f940f321c440fe15
Louis Maddox <louismmx@gmail.com>
2024-06-22 01:10:23 +0100

    feat: add endpoints

--- src/tb8/main.py
+++ src/tb8/main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
 import os
+import tubeulator
 
 app = FastAPI()
 port = int(os.environ.get("PORT", 4000))
 
 @app.get("/")
 def read_root():
-    return {"Hello": "world"}
+    return {"🚨": "It's time for the tubeulator"}
 
-@app.get("/tubeulate")
-def read_root():
-    import tubeulator
-    return {"Hello": "world"}
+@app.get("/lines")
+def read_tubeulate_lines():
+    lines = tubeulator.load_lines()
+    return lines.to_dicts()
+
+@app.get("/lines-by-station")
+def read_tubeulate_lines_by_station():
+    lines = tubeulator.load_lines_by_station()
+    return lines.to_dicts()
+
+@app.get("/stations")
+def read_tubeulate_stations():
+    stations = tubeulator.load_stations()
+    return stations.to_dicts()
 
 def serve():
     import uvicorn
