629c55559d442335adf122bce4961aa89153d098
Louis Maddox <louismmx@gmail.com>
2024-06-22 14:24:48 +0100

    nest the response

--- src/tb8/main.py
+++ src/tb8/main.py
@@ -2,6 +2,7 @@ import os
 
 import tubeulator as tube
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 port = int(os.environ.get("PORT", 4000))
@@ -11,6 +12,11 @@ lines_by_station = tube.load_lines_by_station()
 stations = tube.load_stations()
 
 
+class Response(BaseModel):
+    query: str
+    results: list[dict]
+
+
 @app.get("/")
 def read_root():
     return {"🚨": "It's time for the tubeulator"}
@@ -18,17 +24,17 @@ def read_root():
 
 @app.get("/lines")
 def read_lines(request: Request, query: str = "SELECT * FROM self;"):
-    return lines.sql(query).to_dicts()
+    return Response(query=query, results=lines.sql(query).to_dicts())
 
 
 @app.get("/lines-by-station")
 def read_lines_by_station(request: Request, query: str = "SELECT * FROM self;"):
-    return lines_by_station.sql(query).to_dicts()
+    return Response(query=query, results=lines_by_station.sql(query).to_dicts())
 
 
 @app.get("/stations")
 def read_stations(request: Request, query: str = "SELECT * FROM self;"):
-    return stations.sql(query).to_dicts()
+    return Response(query=query, results=stations.sql(query).to_dicts())
 
 
 def serve():
